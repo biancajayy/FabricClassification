f8b9a55dc0e472239b120034a8b4c7aac37e11bc
biancajayy <bianca.jay09@gmail.com>
2024-08-05 13:06:58 -0400

    setting up front end

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Meow"
+@app.route('/')
+def index(result=None):
+    # Attempt to get the result from URL parameters, defaulting to None if not present
+    result = request.args.get('result', None)
+    return render_template('index.html', result=result)
